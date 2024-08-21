667689a3d685b1ccbb19d65829889d8ac5526f51
hhi <hhi@voxteneo.com>
2024-08-21 13:16:34 +0700

    first commit

---
+++ xamarin-zebra-tech/MainActivity.cs
@@ -0,0 +1,95 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Threading;
+
+namespace xamarin_zebra_tech
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private static int PERMISSION_REQUEST_CODE = 10;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var btnStart = FindViewById<Button>(Resource.Id.btnStart);
+            btnStart.Click += StartProcess;
+        }
+
+        private async void StartProcess(object sender, EventArgs eventArgs)
+        {
+            new Thread(() =>
+            {
+                Thread.Sleep(500);
+                RunOnUiThread(Print);
+            }).Start();
+        }
+
+        private async void Print()
+        {
+            try
+            {
+                string printerMacAddress = "00:22:58:3C:4A:5B";
+                var printerHelper = new ZebraPrinterHelper(printerMacAddress);
+
+                printerHelper.Connect();
+                printerHelper.PrintText("Hello, Zebra Printer");
+                printerHelper.Disconnect();
+
+
+                //if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.R)
+                //{
+
+                //    var listPermissions = new System.Collections.Generic.List<string>();
+
+                //    const string bl = Manifest.Permission.BluetoothAdmin;
+                //    if (CheckSelfPermission(bl) == (int)Android.Content.PM.Permission.Granted)
+                //    {
+                //        Toast.MakeText(this, "Special permissions granted", ToastLength.Short).Show();
+                //        PrintData();
+                //        return;
+                //    }
+                //    else
+                //    {
+                //        listPermissions.Add(Android.Manifest.Permission.BluetoothAdmin);
+                //    }
+
+                //    RequestPermissions(listPermissions.ToArray(), PERMISSION_REQUEST_CODE);
+                //}
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+            }
+        }
+
+
+        //private void PrintData()
+        //{
+        //    Connection connection = new BluetoothConnection("macAddress");
+        //    try
+        //    {
+        //        connection.Open();
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw;
+        //    }
+        //}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ xamarin-zebra-tech/ZebraPrinterHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using Zebra.Sdk.Comm;
+using Zebra.Sdk.Printer;
+
+namespace xamarin_zebra_tech
+{
+    public class ZebraPrinterHelper
+    {
+        private ZebraPrinter printer;
+        private Connection connection;
+
+        public ZebraPrinterHelper(string macAddress)
+        {
+            // Establish a Bluetooth connection
+            connection = new BluetoothConnection(macAddress);
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                connection.Open();
+                printer = ZebraPrinterFactory.GetInstance(connection);
+                //PrinterSettings settings = printer.GetPrinterSettings();
+                // You can configure printer settings if needed
+            }
+            catch (ConnectionException e)
+            {
+                // Handle exceptions
+                Console.WriteLine($"Connection error: {e.Message}");
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (ConnectionException e)
+            {
+                // Handle exceptions
+                Console.WriteLine($"Disconnection error: {e.Message}");
+            }
+        }
+
+        public void PrintText(string text)
+        {
+            try
+            {
+                if (printer != null)
+                {
+                    // Create a ZPL command for printing text
+                    string zpl = "^XA^FO50,50^ADN,36,20^FD" + text + "^FS^XZ";
+                    connection.Write(Encoding.UTF8.GetBytes(zpl));
+                }
+            }
+            catch (ConnectionException e)
+            {
+                // Handle exceptions
+                Console.WriteLine($"Print error: {e.Message}");
+            }
+        }
+    }
+}
